44d42265971a0fb1a7f680123ef03acfe2465964
Jacob Harrington <jharrington4@gmail.com>
2019-11-12 11:22:57 -0800

    updated header

--- public/todo/TodoApp.js
+++ public/todo/TodoApp.js
@@ -8,7 +8,7 @@ import { getTodos, addTodo, updateTodo, removeTodo } from '../services/todo-api.
 class TodoApp extends Component {
 
     async onRender(dom) {
-        const header = new Header({ title: 'TODO LIST' });
+        const header = new Header({ title: 'DO THESE JACOB' });
         dom.prepend(header.renderDOM());
         
         const main = dom.querySelector('main');
